6054644a61d7da6b8240d832cf4ad8e3f916868a
Andrew Davis <asutherlandavis@googlemail.com>
2016-01-27 00:03:17 +0000

    Initial Push I have some customers (along with animations and controllers) set up and they move to the correct place

---
+++ Assets/Scripts/Customer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Random = UnityEngine.Random;
+
+public class Customer
+{
+    #region Position Constants
+    const int doorX = -15;
+    const int doorY = -10;
+    const int ticketsX = 15;
+    const int ticketsY = 2;
+    #endregion
+
+    int index;
+    int characterNum;
+    public int currentDirection = 0;     // 1 down, 2 up, 3 left, 4 right, 0 still
+
+
+    float posX = 0;
+    float posY = 0;
+    
+
+    bool needsFood;
+    bool needsTickets;
+    bool needsToilet;
+    bool goingToSeats = false;
+
+    int travellingToX = 0;
+    int travellingToY = 0;
+
+    public Customer(int id)
+    {
+
+        needsFood = Random.Range(0, 10) >= 4;
+        needsTickets = Random.Range(0, 10) >= 3;
+        needsToilet = Random.Range(0, 10) >= 6;
+                
+        this.nextPlace();
+    }
+
+    public void updatePosition(float x, float y) { posX = x; posY = y; }
+
+
+    public void nextPlace()
+    {
+        if (needsTickets)
+        {
+            travellingToX = ticketsX;
+            travellingToY = ticketsY;
+        }
+
+        // food, toilets go here
+
+        else
+        {
+            travellingToX = doorX;
+            travellingToY = doorY;
+            goingToSeats = true;
+        }
+    }
+
+
+    public int getIndex() { return index; }
+    public float getXCoordinate() { return posX; }
+    public float getYCoordinate() { return posY; }
+    public int getTravellingToX() { return travellingToX; }
+    public int getTravellingToY() { return travellingToY; }
+    public void ticketsDone() { this.needsTickets = false; }
+    public bool isGoingToSeat() { return this.goingToSeats; }
+}
---
+++ Assets/Scripts/movementScript.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movementScript : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private Animator animator;
+
+    public Customer[] customers = new Customer[5];
+
+    public Animation anim;
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+        animator = GetComponent<Animator>();
+        customers[0] = new Customer(0);
+    }
+
+    private void moveCustomer(int index)
+    {
+
+        float theX = transform.position.x;
+        float theY = transform.position.y;
+
+        string direction = "idle";
+
+        //get direction
+        int newDir;
+
+        if (theY > customers[index].getTravellingToY() + 0.11f)
+        {
+            // move up
+            transform.Translate(new Vector3(0, -moveSpeed, 0));
+            //change direction
+            newDir = 1;
+            customers[index].updatePosition(theX, theY - moveSpeed);
+            direction = "down";
+        }
+        else if (theY < customers[index].getTravellingToY() - 0.11f)
+        {
+            // move down
+            transform.Translate(new Vector3(0, moveSpeed, 0));
+            //change direction
+            newDir = 2;
+            customers[index].updatePosition(theX, theY + moveSpeed);
+            direction = "up";
+        }
+        else if (theX < customers[index].getTravellingToX() - 0.11f)
+        {
+            // move left
+            transform.Translate(new Vector3(+moveSpeed, 0, 0));
+            //change direction
+            newDir = 4;
+            customers[index].updatePosition(theX + moveSpeed, theY);
+            direction = "right";
+        }
+        else if (theX > customers[index].getTravellingToX() + 0.11f)
+        {
+            // move right
+            transform.Translate(new Vector3(-moveSpeed, 0, 0));
+            //change direction
+            newDir = 3;
+            customers[index].updatePosition(theX - moveSpeed, theY);
+            direction = "left";
+        }
+        else
+        {
+            newDir = 0;
+            customers[index].nextPlace();
+
+            //if (!customers[index].isGoingToSeat())
+            //{
+            //    customerTimers[index].Stop();
+            //    customers[index].getPicture().Top = 300 + (ticketsQueue.Count * 50);
+            //    customers[index].ticketsDone();
+            //    customers[index].nextPlace();
+            //    customers[index].getPicture().Left = 520;
+
+            //    ticketsQueue.Enqueue(new Customer(index));
+            //}
+            //else
+            //{
+            //    customerTimers[index].Stop();
+            //    customers[index].getPicture().Visible = false;
+            //}
+        }
+
+        //Debug.Log("DIRECTION: " + direction);
+
+
+        if (customers[index].currentDirection != newDir)
+        {
+            // change image
+
+            Debug.Log(direction);
+            //GetComponent<animation>.Play("down");
+            //GetComponent<Animation>().Play(direction);
+            //animation.Play("down");
+
+            animator.SetTrigger(direction);
+
+        }
+        
+        customers[index].currentDirection = newDir;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        moveCustomer(0);        
+        
+    }
+}
